e3faa7fdaa9e32d6599e371da9d6929e7709700c
freitaspedro <pvpfreitas@gmail.com>
2022-01-19 15:58:02 -0300

    add controller mvc

--- app/src/main/java/com/example/androidarchitectures/mvc/MVCActivity.java
+++ app/src/main/java/com/example/androidarchitectures/mvc/MVCActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.androidarchitectures.R;
@@ -19,12 +22,17 @@ public class MVCActivity extends AppCompatActivity {
     private List<String> values = new ArrayList<>();
     private ArrayAdapter<String> adapter;
     private ListView listView;
+    private CountriesController controller;
+    private Button retryButton;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mvc);
         setTitle(getString(R.string.title_mvc));
+
+        controller = new CountriesController(this);
 
         listView = findViewById(R.id.listView);
         adapter = new ArrayAdapter<>(this, R.layout.item_layout, R.id.textView, values);
@@ -34,44 +42,33 @@ public class MVCActivity extends AppCompatActivity {
                 Toast.makeText(this, "You clicked " + values.get(position), Toast.LENGTH_SHORT).show()
         );
 
-        ArrayList<String> mockValues = new ArrayList<>();
-        mockValues.add("USA");
-        mockValues.add("UK");
-        mockValues.add("France");
-        mockValues.add("Italy");
-        mockValues.add("China");
-        mockValues.add("Japan");
-        mockValues.add("USA");
-        mockValues.add("UK");
-        mockValues.add("France");
-        mockValues.add("Italy");
-        mockValues.add("China");
-        mockValues.add("Japan");
-        mockValues.add("USA");
-        mockValues.add("UK");
-        mockValues.add("France");
-        mockValues.add("Italy");
-        mockValues.add("China");
-        mockValues.add("Japan");
-        mockValues.add("USA");
-        mockValues.add("UK");
-        mockValues.add("France");
-        mockValues.add("Italy");
-        mockValues.add("China");
-        mockValues.add("Japan");
-        mockValues.add("USA");
-        mockValues.add("UK");
-        mockValues.add("France");
-        mockValues.add("Italy");
-        mockValues.add("China");
-        mockValues.add("Japan");
-        setValues(mockValues);
+        retryButton = findViewById(R.id.retryButton);
+        retryButton.setOnClickListener(this::onRetry);
+
+        progressBar = findViewById(R.id.progressBar);
     }
 
     public void setValues(List<String> values) {
         this.values.clear();
         this.values.addAll(values);
+        retryButton.setVisibility(View.GONE);
+        progressBar.setVisibility(View.GONE);
+        listView.setVisibility(View.VISIBLE);
         adapter.notifyDataSetChanged();
+    }
+
+    public void onRetry(View view) {
+        controller.onRefresh();
+        retryButton.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+        listView.setVisibility(View.GONE);
+    }
+
+    public void onError() {
+        Toast.makeText(this, getString(R.string.error_message), Toast.LENGTH_SHORT).show();
+        retryButton.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.GONE);
+        listView.setVisibility(View.GONE);
     }
 
     public static Intent getIntent(Context context) {
