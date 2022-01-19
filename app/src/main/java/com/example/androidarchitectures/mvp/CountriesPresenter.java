fb7711f7a73e9135e9ee9f5e7e8e43d9480383d5
freitaspedro <pvpfreitas@gmail.com>
2022-01-19 16:33:07 -0300

    add mvp

---
+++ app/src/main/java/com/example/androidarchitectures/mvp/CountriesPresenter.java
@@ -0,0 +1,54 @@
+package com.example.androidarchitectures.mvp;
+
+import com.example.androidarchitectures.model.CountriesService;
+import com.example.androidarchitectures.model.Country;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class CountriesPresenter {
+
+    private View view;
+    private CountriesService service;
+
+    public CountriesPresenter(View view) {
+        this.view = view;
+        service = new CountriesService();
+        fetchCountries();
+    }
+
+    private void fetchCountries() {
+        service.getCountries()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableSingleObserver<List<Country>>() {
+                    @Override
+                    public void onSuccess(List<Country> value) {
+                        List<String> countryNames = new ArrayList<>();
+                        for (Country country : value) {
+                            countryNames.add(country.name);
+                        }
+                        view.setValues(countryNames);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        view.onError();
+                    }
+                });
+    }
+
+    public void onRefresh() {
+        fetchCountries();
+    }
+
+    public interface View {
+        void setValues(List<String> countries);
+        void onError();
+    }
+
+}
--- app/src/main/java/com/example/androidarchitectures/mvp/MVPActivity.java
+++ app/src/main/java/com/example/androidarchitectures/mvp/MVPActivity.java
@@ -5,21 +5,77 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.androidarchitectures.R;
+import com.example.androidarchitectures.mvc.CountriesController;
 import com.example.androidarchitectures.mvc.MVCActivity;
 
-public class MVPActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MVPActivity extends AppCompatActivity implements CountriesPresenter.View {
+
+    private List<String> values = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    private ListView listView;
+    private CountriesPresenter presenter;
+    private Button retryButton;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mvp);
         setTitle(getString(R.string.title_mvp));
+
+        presenter = new CountriesPresenter(this);
+
+        listView = findViewById(R.id.listView);
+        adapter = new ArrayAdapter<>(this, R.layout.item_layout, R.id.textView, values);
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener((parent, view, position, id) ->
+                Toast.makeText(this, "You clicked " + values.get(position), Toast.LENGTH_SHORT).show()
+        );
+
+        retryButton = findViewById(R.id.retryButton);
+        retryButton.setOnClickListener(this::onRetry);
+
+        progressBar = findViewById(R.id.progressBar);
+    }
+
+    @Override
+    public void setValues(List<String> values) {
+        this.values.clear();
+        this.values.addAll(values);
+        retryButton.setVisibility(View.GONE);
+        progressBar.setVisibility(View.GONE);
+        listView.setVisibility(View.VISIBLE);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void onRetry(View view) {
+        presenter.onRefresh();
+        retryButton.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+        listView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onError() {
+        Toast.makeText(this, getString(R.string.error_message), Toast.LENGTH_SHORT).show();
+        retryButton.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.GONE);
+        listView.setVisibility(View.GONE);
     }
 
     public static Intent getIntent(Context context) {
         return new Intent(context, MVPActivity.class);
     }
-
 }
